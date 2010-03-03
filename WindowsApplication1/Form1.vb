0badbb131584b95a49c081ec2e63580a7146b186
tperales <tperales@lgengineers.com>
2010-03-03 14:16:57 -0600

    First testing for automatic fixed form size on open

--- WindowsApplication1/Form1.vb
+++ WindowsApplication1/Form1.vb
@@ -7,10 +7,13 @@
     Dim htFCtoFLAG As Hashtable
     Dim conn As New SqlClient.SqlConnection("Data Source=missas01;Initial Catalog=GeneralizedScope;Persist Security Info=True;User ID=sa;Password=sa")
 
-    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cbFC110AllC.AutoSizeChanged
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         Dim doc_id As Integer
         Dim fc_id As Integer
 
+        'testing formsize 
+        Dim formsize As New Size(1000, 900)
+        Me.MaximumSize() = formsize
 
         doc_id = 4
 
@@ -38,7 +41,7 @@
         Dim init_parent_id = 0
         For fc_id = 1 To 9
             PopulateDataView(init_parent_id, fc_id)
-            'PopulateFlagsView(fc_id, doc_id)
+            PopulateFlagsView(fc_id, doc_id)
         Next
     End Sub
 
