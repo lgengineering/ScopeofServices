2106b19fd77aee125b3b3a3718283734b96b8ea4
pgarza <pgarza@lgengineers.com>
2010-03-03 09:53:02 -0600

    test merge

--- WindowsApplication1/Form1.vb
+++ WindowsApplication1/Form1.vb
@@ -1,7 +1,6 @@
 ﻿Public Class Form1
     'test comment 2
     'test comment
-    'test comment 3
     'A globally unique Identifier.  Each Attachment_B document gets one
     Dim GUID As String
     Dim htFCtoSVC As Hashtable
